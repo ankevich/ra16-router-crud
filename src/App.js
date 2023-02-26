e53b43734b73d60dd73f9f94894c70650accb445
Annie <overheads165@gmail.com>
2023-02-26 20:36:35 +0100

    NewPost styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
     return (
@@ -14,17 +15,29 @@ function App() {
 
 const NewPost = () => {
     return (
-        <div className="post-form">
-            <div className="post-form__actions">
-                <NavLink className="menu__item" to="/">
-                    ❌
-                </NavLink>
-                <textarea className="post-form__textarea" placeholder="Введите текст поста" />
-                <button className="post-form__submit">Опубликовать</button>
-            </div>
-        </div>
+        <StyledNewPost>
+            <NavLink className="menu__item" to="/">
+                ❌
+            </NavLink>
+            <textarea className="post-form__textarea" placeholder="Введите текст поста" />
+            <button className="post-form__submit">Опубликовать</button>
+        </StyledNewPost>
     );
 };
+
+const StyledNewPost = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    .post-form__textarea {
+        height: 100px;
+        margin: 10px 0;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        resize: none;
+    }
+`;
 
 const HomePage = () => {
     return (
@@ -61,6 +74,6 @@ const ViewPost = () => {
             </div>
         </div>
     );
-}
+};
 
 export default App;

d67efd1c44b06429a603875a2fe2334dfa64d253
Annie <overheads165@gmail.com>
2023-02-26 20:18:47 +0100

    ViewPost

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-do
 
 function App() {
     return (
-        // список постов
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/posts/new" element={<NewPost />} />
+                <Route path="/posts/:id" element={<ViewPost />} />
             </Routes>
         </Router>
     );
@@ -39,12 +39,28 @@ const HomePage = () => {
             <div className="posts">
                 <div className="post">
                     <div className="post__content">
-                        <div className="post__text">Текст поста</div>
+                        <NavLink className="post__text" to="/posts/1">
+                            Текст поста
+                        </NavLink>
                     </div>
                 </div>
             </div>
         </>
     );
 };
+
+const ViewPost = () => {
+    return (
+        <div className="post">
+            <div className="post__content">
+                <div className="post__text">Текст поста</div>
+            </div>
+            <div className="post__actions">
+                <button className="post__delete">❌</button>
+                <button className="post__edit">Редактировать</button>
+            </div>
+        </div>
+    );
+}
 
 export default App;
